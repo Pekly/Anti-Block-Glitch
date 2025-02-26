cc3d1f95751215cc7fe10e1b39ae53d4d24725cf
Pekly <92947595+Pekly@users.noreply.github.com>
2025-02-26 23:34:42 +0800

    Update Anticheat.java

--- src/main/java/me/Ice/antiblockglitch/Anticheat.java
+++ src/main/java/me/Ice/antiblockglitch/Anticheat.java
@@ -1,45 +1,40 @@
 package me.Ice.antiblockglitch;
 
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
-import com.sk89q.worldguard.protection.flags.Flags;
-import com.sk89q.worldguard.protection.flags.StateFlag;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Set;
 
 public class Anticheat extends JavaPlugin {
 
     private static Anticheat instance;
+    private String denyMessage;
 
+    @Override
     public void onEnable() {
         instance = this;
+        saveDefaultConfig();  // Saves config if not present
+        reloadDenyMessage();
+
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new CheckerHandler(), this);
     }
 
+    @Override
     public void onDisable() {
-
+        instance = null;
     }
 
     public static Anticheat getInstance() {
         return instance;
     }
 
+    public void reloadDenyMessage() {
+        FileConfiguration config = getConfig();
+        denyMessage = config.getString("deny-message", "&c&l(!) You are not allowed to place blocks here!");
+    }
+
+    public String getDenyMessage() {
+        return denyMessage;
+    }
 }
