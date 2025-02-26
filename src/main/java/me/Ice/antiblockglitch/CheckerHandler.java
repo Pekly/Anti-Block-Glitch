3eea2606ffb161bb3fb98e5809577b4fab8cc988
Pekly <92947595+Pekly@users.noreply.github.com>
2025-02-26 23:35:02 +0800

    Update CheckerHandler.java

--- src/main/java/me/Ice/antiblockglitch/CheckerHandler.java
+++ src/main/java/me/Ice/antiblockglitch/CheckerHandler.java
@@ -6,76 +6,56 @@ import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionContainer;
 import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-import com.sk89q.worldguard.protection.regions.RegionContainer;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.util.Set;
 
 public class CheckerHandler implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        BlockChecker checker = BlockChecker.getBlockChecker(e.getPlayer());
-        checker.runBlockChecker();
-    }
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (player.hasPermission("antiblockglitch.override")) return;
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent e) {
-        BlockChecker checker = BlockChecker.getBlockChecker(e.getPlayer());
-        checker.cancelBlockChecker();
-    }
-
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent e) {
-        Player p = e.getPlayer();
-        if (p.hasPermission("antiblockglitch.override")) {
-            return;
-        }
+        Location location = event.getBlockPlaced().getLocation();
+        World world = location.getWorld();
+        if (world == null) return;
 
         RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
-        RegionManager manager = container.get(BukkitAdapter.adapt(p.getWorld()));
+        RegionManager manager = container.get(BukkitAdapter.adapt(world));
         if (manager == null) return;
 
-        Location loc = e.getBlockPlaced().getLocation();
-        ApplicableRegionSet regions = manager.getApplicableRegions(BlockVector3.at(loc.getX(), loc.getY(), loc.getZ()));
-        Set<ProtectedRegion> set = regions.getRegions();
+        ApplicableRegionSet regions = manager.getApplicableRegions(BlockVector3.at(location.getX(), location.getY(), location.getZ()));
 
-        boolean cancel = false;
-        for (ProtectedRegion rg : set) {
-            StateFlag.State blockPlaceFlag = rg.getFlags().getOrDefault(Flags.BLOCK_PLACE, StateFlag.State.ALLOW);
-            StateFlag.State buildFlag = rg.getFlags().getOrDefault(Flags.BUILD, StateFlag.State.ALLOW);
-
-            if (blockPlaceFlag == StateFlag.State.DENY || buildFlag == StateFlag.State.DENY) {
-                if (!rg.getMembers().contains(p.getUniqueId())) {
-                    cancel = true;
-                    break;
-                }
-            }
-        }
+        boolean cancel = regions.getRegions().stream().anyMatch(region -> 
+            (region.getFlags().getOrDefault(Flags.BLOCK_PLACE, StateFlag.State.ALLOW) == StateFlag.State.DENY ||
+            region.getFlags().getOrDefault(Flags.BUILD, StateFlag.State.ALLOW) == StateFlag.State.DENY) &&
+            !region.getMembers().contains(player.getUniqueId()));
 
         if (cancel) {
-            e.setCancelled(true);
-            e.getBlockPlaced().setType(Material.AIR);
-            BlockChecker checker = BlockChecker.getBlockChecker(p);
-            p.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "(!) You are not allowed to place blocks here!");
+            event.setCancelled(true);
+            event.getBlockPlaced().setType(Material.AIR);
 
+            BlockChecker checker = BlockChecker.getBlockChecker(player);
             Location lastBlock = checker.getLastStoodBlock();
-            if (!loc.equals(lastBlock)) {
-                Location safeLoc = lastBlock.clone().add(0, 1, 0);
-                if (!safeLoc.getBlock().getType().isSolid()) {
-                    p.teleport(safeLoc);
+            if (lastBlock == null || lastBlock.equals(location)) return;
+
+            Bukkit.getScheduler().runTaskLater(Anticheat.getInstance(), () -> {
+                if (player.isOnline()) {
+                    player.teleport(lastBlock.add(0, 1, 0));
                 }
-            }
+            }, 1L);
+
+            String denyMessage = ChatColor.translateAlternateColorCodes('&', Anticheat.getInstance().getDenyMessage());
+            player.sendMessage(denyMessage);
         }
     }
 }
